6740bcba008546327f53ece4edb50dea9f633f98
iamfarooqi <attaurrahmanfarooqi9@gmail.com>
2020-12-11 17:18:14 +0500

    done

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+let choice = "y";
+do {
+    let num1 = +prompt("Enter 1st Number");
+    let num2 = +prompt("Enter 2nd Number");
+    var num3 = num1 + num2;
+
+   alert(num3);
+
+    
+
+
+    
+    
+}while(choice === prompt("Do You Want Again y/n "  ));
